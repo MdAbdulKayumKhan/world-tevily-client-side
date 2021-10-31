4509361c1397507057f20d16fe82d8524ca2078d
MdAbdulKayumKhan <kayum261@gmail.com>
2021-10-31 21:58:27 +0600

    spinner added on home page

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -7,7 +7,7 @@ import './Home.css';
 
 const Home = () => {
     const [services, setServices] = useState([]);
-    const [isLoadingHome, setIsLoadingHome] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const { user } = useAuth();
 
 
@@ -17,10 +17,11 @@ const Home = () => {
             .then(servicesData => {
 
                 setServices(servicesData)
+                setIsLoading(false)
 
             })
 
-    }, [])
+    }, [services])
 
     // array sorting
     // const reverseOrder = services.reverse();
@@ -38,25 +39,31 @@ const Home = () => {
 
             <div className="container mb-5">
                 <div className="card-deck">
-                    <div className="row gx-5">
-                        {
-                            mainOffers.map(service => (
-                                <div key={service._id} className="card col-12 col-sm-12 col-md-4 col-lg-4 gap-4">
-                                    <img className="card-img-top img-fluid" src={service.img} alt="Card cap" />
-                                    <div className="card-body">
-                                        <h5 className="card-title">{service.name}</h5>
-                                        <p className="card-text">{service.description.slice(0, 120)}.</p>
-                                        <p className="card-text bg-light fs-5">Duration: {service.days}</p>
-                                        <p className="card-text bg-light fs-5">Price for the tour (Per Person):৳{service.offer}</p>
+                    {isLoading ? 
+                        <Spinner animation="grow" variant="info" />
+                     : (
+                        <div className="row gx-5">
+                            {
+                                mainOffers.map(service => (
+                                    <div key={service._id} className="card col-12 col-sm-12 col-md-4 col-lg-4 gap-4">
+                                        <img className="card-img-top img-fluid" src={service.img} alt="Card cap" />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{service.name}</h5>
+                                            <p className="card-text">{service.description.slice(0, 120)}.</p>
+                                            <p className="card-text bg-light fs-5">Duration: {service.days}</p>
+                                            <p className="card-text bg-light fs-5">Price for the tour (Per Person):৳{service.offer}</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted"> <Link to={`/servicesDetails/${service._id}`}> <Button variant="dark">Book Now</Button></Link></small>
+                                        </div>
                                     </div>
-                                    <div className="card-footer">
-                                        <small className="text-muted"> <Link to={`/servicesDetails/${service._id}`}> <Button variant="dark">Book Now</Button></Link></small>
-                                    </div>
-                                </div>
-                            ))
-                        }
+                                ))
+                               
+                            }
 
-                    </div>
+                        </div> )
+                         
+                        }
                 </div>
             </div>
             <div>
